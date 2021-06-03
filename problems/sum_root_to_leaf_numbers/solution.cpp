674337759980ef4f0ff23565ae17cacc5e9b4761
georgetree <46710574+georgetree@users.noreply.github.com>
2021-06-03 10:20:39 +0000

    Sync LeetCode submission - Sum Root to Leaf Numbers (cpp)

---
+++ problems/sum_root_to_leaf_numbers/solution.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int sumNumbers(TreeNode* root) {
+        return sumNum(root,0);
+    }
+    int sumNum(TreeNode *root, int sum){
+        if(!root) return 0;
+        sum = 10 * sum + root->val;
+        if(!root->left && !root->right) return sum;
+        return sumNum(root->left,sum) + sumNum(root->right,sum);
+    }
+};
