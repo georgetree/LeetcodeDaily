d61a16fe189b66260e050aacb84c64c29cd3d37e
georgetree <46710574+georgetree@users.noreply.github.com>
2022-02-18 17:25:28 +0000

    Sync LeetCode submission - Remove Duplicate Letters (cpp)

---
+++ problems/remove_duplicate_letters/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        unordered_map<char,int> m;
+        string res = "";
+        for(auto i:s){
+            m[i]+=1;
+        }
+        for(auto i:s){
+            m[i]-=1;
+            if(res.find(i) != string::npos) continue;
+            while(m[res.back()] > 0 && !res.empty() && res.back() > i ){
+                res.pop_back();
+            }
+            res.push_back(i);
+        }
+        return res;
+    }
+};
