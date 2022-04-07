3ae43f942c971c9b453ca6f84beb3d05b5dd7c64
georgetree <46710574+georgetree@users.noreply.github.com>
2022-04-07 16:59:15 +0000

    Sync LeetCode submission - Last Stone Weight (cpp)

--- problems/last_stone_weight/solution.cpp
+++ problems/last_stone_weight/solution.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
     int lastStoneWeight(vector<int>& stones) {
-            priority_queue<int> MaxHeap(stones.begin(), stones.end());
-            while(MaxHeap.size()>1){
-                int a = MaxHeap.top();
-                MaxHeap.pop();
-                int b = MaxHeap.top();
-                MaxHeap.pop();
-                if(abs(a-b)!=0)
-                    MaxHeap.push(abs(a-b));
+        priority_queue<int> q;
+        for(auto i:stones)
+            q.push(i);
+        while(q.size()>1){
+            int t1 = q.top();
+            q.pop();
+            int t2 = q.top();
+            q.pop();
+            if(t1==t2) continue;
+            else if(t1!=t2){
+                t1 = t1-t2;
+                q.push(t1);
             }
-            if(!MaxHeap.empty())
-                return MaxHeap.top();
-            else return 0;
+        }
+        return q.size()==1 ? q.top() : 0;
     }
 };
