3de51714b4b32e5eee1ff356b69f28d272094d95
georgetree <46710574+georgetree@users.noreply.github.com>
2022-04-28 15:04:35 +0000

    Sync LeetCode submission - Path With Minimum Effort (python3)

---
+++ problems/path_with_minimum_effort/solution.py
@@ -0,0 +1,24 @@
+class Solution:
+    def minimumEffortPath(self, heights: List[List[int]]) -> int:
+        m, n = len(heights), len(heights[0])
+        q = [(0, 0, 0)]
+        dist = [0] + [float("inf")] * (m * n - 1)
+        seen = set()
+
+        while q:
+            d, x, y = heapq.heappop(q)
+            iden = x * n + y
+            if iden in seen:
+                continue
+            if (x, y) == (m - 1, n - 1):
+                break
+            
+            seen.add(iden)
+            for nx, ny in [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)]:
+                if 0 <= nx < m and 0 <= ny < n and max(d, abs(heights[x][y] - heights[nx][ny])) <= dist[nx * n + ny]:
+                    dist[nx * n + ny] = max(d, abs(heights[x][y] - heights[nx][ny]))
+                    heapq.heappush(q, (dist[nx * n + ny], nx, ny))
+        
+        return dist[m * n - 1]
+
+ 
