b8451db3d9fa1b278f2cc553445854815ac91d6a
georgetree <46710574+georgetree@users.noreply.github.com>
2022-05-10 12:39:09 +0000

    Sync LeetCode submission - Combination Sum III (cpp)

--- problems/combination_sum_iii/solution.cpp
+++ problems/combination_sum_iii/solution.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
     vector<vector<int>> combinationSum3(int k, int n) {
+
         vector<vector<int>> res;
-        vector<int> numlist;
-        combinSum(res,numlist,k,n,1);
+        vector<int> combin;
+        helper(res,combin,k,n,1);
         return res;
     }
-    void combinSum(vector<vector<int>> &res,vector<int> &numlist, int k, int n, int start){
-        if(numlist.size()==k || n<=0){ 
-            if(numlist.size()==k && n==0){
-                res.push_back(numlist);
-            }
+    
+    void helper(vector<vector<int>> &res, vector<int> &combin,int k, int n, int start){
+        if(n<=0 || combin.size()==k){
+            if(n==0 && combin.size()==k)
+                res.push_back(combin);
             return;
         }
         for(int i=start; i<=9; i++){
-            numlist.push_back(i);
-            combinSum(res,numlist,k,n-i,i+1);
-            numlist.pop_back();
+            combin.push_back(i);
+            helper(res,combin,k,n-i,i+1);
+            combin.pop_back();
         }
     }
 };
