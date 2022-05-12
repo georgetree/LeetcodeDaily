70af9b47916e3379e8b12db940383c36496a1ff6
georgetree <46710574+georgetree@users.noreply.github.com>
2022-05-12 06:53:59 +0000

    Sync LeetCode submission - Permutations II (cpp)

--- problems/permutations_ii/solution.cpp
+++ problems/permutations_ii/solution.cpp
@@ -4,21 +4,22 @@ public:
         vector<vector<int>> result;
         vector<int> com;
         sort(nums.begin(),nums.end());
+
         vector<int> flag(nums.size(),0);
-        dfs(result,com,nums,flag,0);
+        dfs(result,com,nums,flag);
         return result;
     }
-    void dfs(vector<vector<int>> &result, vector<int> &com, vector<int> &n, vector<int> &flag,int start){
+    void dfs(vector<vector<int>> &result, vector<int> &com, vector<int> &n, vector<int> &flag){
         if(com.size()==n.size()){ 
             result.push_back(com);
             return;
         }
         for(int i=0; i < n.size(); i++){
             if(flag[i]) continue; 
-            if(i>0 && n[i]==n[i-1] && flag[i-1]==0) continue;
+            if( i > 0 && flag[i-1]==0 && n[i] == n[i - 1]) continue;
             flag[i] = 1;
             com.push_back(n[i]);
-            dfs(result,com,n,flag,i+1);
+            dfs(result,com,n,flag);
             com.pop_back();
             flag[i] = 0;
         }
