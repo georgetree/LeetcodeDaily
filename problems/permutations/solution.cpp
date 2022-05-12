b90faccf60cdcc7fbdc36330202002eab3f3f10a
georgetree <46710574+georgetree@users.noreply.github.com>
2022-05-12 06:50:05 +0000

    Sync LeetCode submission - Permutations (cpp)

--- problems/permutations/solution.cpp
+++ problems/permutations/solution.cpp
@@ -4,10 +4,10 @@ public:
         vector<vector<int>> result;
         vector<int> com;
         vector<int> flag(nums.size(),0);
-        dfs(result,com,nums,flag,0);
+        dfs(result,com,nums,flag);
         return result;
     }
-    void dfs(vector<vector<int>> &result, vector<int> &com, vector<int> &n, vector<int> &flag,int start){
+    void dfs(vector<vector<int>> &result, vector<int> &com, vector<int> &n, vector<int> &flag){
         if(com.size()==n.size()){ 
             result.push_back(com);
             return;
@@ -16,7 +16,7 @@ public:
             if(flag[i]) continue; 
             flag[i] = 1;
             com.push_back(n[i]);
-            dfs(result,com,n,flag,i+1);
+            dfs(result,com,n,flag);
             com.pop_back();
             flag[i] = 0;
         }
