ae98c7942c4f4799382685dde0938744a38a67b0
georgetree <46710574+georgetree@users.noreply.github.com>
2022-07-04 07:49:36 +0000

    Sync LeetCode submission - Candy (cpp)

--- problems/candy/solution.cpp
+++ problems/candy/solution.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     int candy(vector<int>& ratings) {
-        vector<int> num(ratings.size(),1);
-        for(int i=1; i < ratings.size(); i++){
-            if(ratings[i] > ratings[i-1])
-                num[i] = num[i-1] + 1;
+        vector<int> res (ratings.size(),1);
+        for(int i=1; i< ratings.size(); i++){
+            if(ratings[i] > ratings[i-1]) res[i] = res[i-1]+1;
         }
         for(int k=ratings.size()-1; k>0; k--){
-            if(ratings[k-1] > ratings[k] )
-                num[k-1] = max(num[k-1]  , num[k]+1);
+            if(ratings[k] < ratings[k-1]) res[k-1] = max(res[k]+1, res[k-1]);
         }
-        return accumulate(num.begin(),num.end(),0);
+        return accumulate(res.begin(),res.end(),0);
     }
 };
