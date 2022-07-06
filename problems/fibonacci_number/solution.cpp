cb94ede25a2073da007e68887b4e73a06d09cb1d
georgetree <46710574+georgetree@users.noreply.github.com>
2022-07-06 13:25:44 +0000

    Sync LeetCode submission - Fibonacci Number (cpp)

--- problems/fibonacci_number/solution.cpp
+++ problems/fibonacci_number/solution.cpp
@@ -1,16 +1,9 @@
 class Solution {
 public:
-    int fib(int N) {
-        int a = 0 , b = 1, c = 0;
-        if(N==0) return 0;
-        if(N==1) return 1;
-        else{
-            for(int i=2;i<=N;++i){
-                c = a + b;
-                a = b; 
-                b = c;
-            }
-        }
-        return c;
+    int fib(int n) {
+        if(n==0) return 0;
+        if(n==1) return 1;
+        
+        return  fib(n-2)+fib(n-1);
     }
 };
