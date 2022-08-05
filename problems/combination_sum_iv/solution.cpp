debb1e58dcdf070a102f61d87d88676c8d3a7f59
georgetree <46710574+georgetree@users.noreply.github.com>
2022-08-05 13:38:41 +0000

    Sync LeetCode submission - Combination Sum IV (cpp)

--- problems/combination_sum_iv/solution.cpp
+++ problems/combination_sum_iv/solution.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     int combinationSum4(vector<int>& nums, int target) {
-        //dp[i] = dp[target-nums[i]](i>=0 && i < nums.size())
-        vector<unsigned int> dp(target+1,0);
-        dp[0] = 1; //num剛好等於target 
+        
+        vector<uint> dp(target+1,0);
+        dp[0] = 1;
         for(int i=1; i<=target; i++){
-            for(int num:nums){
-                if(num > i) continue;
+            for(auto const &num:nums){
+                if(num>i) continue;
                 dp[i] += dp[i-num];
             }
         }
-        return dp[target];
+        return dp.back();
     }
 };
