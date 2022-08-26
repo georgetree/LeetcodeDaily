9d36d2bb199e069f26d1dc62bfb256f670d622a5
georgetree <46710574+georgetree@users.noreply.github.com>
2022-08-26 06:46:44 +0000

    Sync LeetCode submission - Valid Anagram (cpp)

--- problems/valid_anagram/solution.cpp
+++ problems/valid_anagram/solution.cpp
@@ -1,15 +1,8 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.length()!=t.length()) return false;
-        unordered_map<char,int> m;
-        for(auto i:s){
-            m[i]++;
-        }
-        for(auto j:t){
-            if(m[j]>=1) m[j]--;
-            else return false;
-        }
-        return true;
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        return s==t;
     }
 };
