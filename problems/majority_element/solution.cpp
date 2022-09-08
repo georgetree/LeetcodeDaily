d11edbb528fc257167243c3ca96b0c5418ee7946
georgetree <46710574+georgetree@users.noreply.github.com>
2022-09-08 12:40:45 +0000

    Sync LeetCode submission - Majority Element (cpp)

--- problems/majority_element/solution.cpp
+++ problems/majority_element/solution.cpp
@@ -1,18 +1,12 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int res = nums[0];
-        int count = 1;
-        for(int i=1; i<nums.size(); i++){
-            if(res == nums[i])
-                count++;
-            else if(count > 0)
-                count --;
-            else{
-                res = nums[i];
-            }
-                
+        unordered_map<int,int>m;
+        for(auto n:nums){
+            m[n]++;
+            if(m[n]>nums.size()/2) return n;
         }
-        return res;
+        return 0;
+
     }
 };
