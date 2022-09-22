987efb332c1effacab356a89425497925088ccf3
georgetree <46710574+georgetree@users.noreply.github.com>
2022-09-22 15:32:05 +0000

    Sync LeetCode submission - Climbing Stairs (cpp)

--- problems/climbing_stairs/solution.cpp
+++ problems/climbing_stairs/solution.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        if(n<2) return n;
         vector<int> dp(n+1,1);
         for(int i=2; i<=n; i++){
             dp[i] = dp[i-1] + dp[i-2];
