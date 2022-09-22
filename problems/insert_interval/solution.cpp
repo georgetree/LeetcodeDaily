476c284dde490b953bf32d51ed7d7e572b17e377
georgetree <46710574+georgetree@users.noreply.github.com>
2022-09-22 17:59:20 +0000

    Sync LeetCode submission - Insert Interval (cpp)

---
+++ problems/insert_interval/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        vector<vector<int>> res;
+        int pos = 0;
+        for(int i=0; i<intervals.size(); ++i){
+            if(intervals[i][0] > newInterval[1])
+                res.push_back(intervals[i]);
+            else if(intervals[i][1] < newInterval[0]){
+                res.push_back(intervals[i]);
+                ++pos;
+            }
+            else{
+                newInterval[0] = min(intervals[i][0],newInterval[0]);
+                newInterval[1] = max(intervals[i][1],newInterval[1]);
+            }
+        }
+        res.emplace(res.begin()+pos,newInterval);
+        
+        return res;
+    }
+};
