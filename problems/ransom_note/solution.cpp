c394990d44e5a9d01900faaab537d4a40178f7f8
georgetree <46710574+georgetree@users.noreply.github.com>
2022-09-22 15:28:05 +0000

    Sync LeetCode submission - Ransom Note (cpp)

--- problems/ransom_note/solution.cpp
+++ problems/ransom_note/solution.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-        vector<int> v(26,0);
-        for(char c:magazine)
-            v[c-'a']++;
-        for(char c:ransomNote){
-            if(v[c-'a']!=0) v[c-'a']--;
+        unordered_map<char,int>m;
+        for(auto &s: magazine)
+            m[s]++;
+        for(auto &r:ransomNote){
+            if(m[r]>0)
+                m[r]--;
             else return false;
         }
         return true;
