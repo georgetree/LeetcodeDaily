ceceb987902baa74e138526fb4759935e2d10888
georgetree <46710574+georgetree@users.noreply.github.com>
2022-09-24 15:06:24 +0000

    Sync LeetCode submission - Clone Graph (cpp)

--- problems/clone_graph/solution.cpp
+++ problems/clone_graph/solution.cpp
@@ -22,16 +22,22 @@ public:
 class Solution {
 public:
     Node* cloneGraph(Node* node) {
+        if(node==nullptr) return{};
+        queue<Node*>q;
+        q.push(node);
         unordered_map<Node*,Node*> m;
-        return help(node,m);
-    }
-    Node* help(Node* node,unordered_map<Node*,Node*> &m){
-        if(node==NULL) return node;
-        if(m.find(node) != m.end()) return m[node];
-        Node* clone = new Node(node->val);
+        Node *clone = new Node(node->val);
         m[node] = clone;
-        for(Node *t : node->neighbors){
-            clone->neighbors.push_back(help(t,m));
+        while(!q.empty()){
+            Node* t = q.front();
+            q.pop();
+            for(Node* temp : t->neighbors){
+                if(m.count(temp)!=1){
+                    q.push(temp);
+                    m[temp] = new Node(temp->val);
+                }
+                m[t]->neighbors.push_back(m[temp]);
+            }
         }
         return clone;
     }
