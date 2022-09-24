486fb7cefea7c95992c7534f64c257deb1f603f6
georgetree <46710574+georgetree@users.noreply.github.com>
2022-09-24 18:12:50 +0000

    Sync LeetCode submission - Coin Change (cpp)

--- problems/coin_change/solution.cpp
+++ problems/coin_change/solution.cpp
@@ -1,32 +1,14 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        if(amount==0) return 0;
-        int res = 0;
-        queue<int> q;
-        q.push(amount);
-        sort(coins.begin(),coins.end());
-        vector<int> v(amount+1,0);
-        v[amount] = true;
-        while(!q.empty()){
-            res++;
-            for(int i = q.size(); i>0; i--){
-                int h = q.front();
-                q.pop();
-                for(auto coin:coins){
-                    int n = h - coin;
-                    if(n==0)
-                        return res;
-                    else if(n<0)
-                        break;
-                    else if(!v[n]){
-                        v[n] = true;
-                        q.push(n);
-                    }
-                }
+        vector<int> dp(amount+1,amount+1);
+        dp[0] = 0;
+        for(int i=0; i<dp.size(); i++){
+            for(int coin :coins){
+                if(i - coin < 0) continue;
+                else dp[i] = min(dp[i],1+dp[i-coin]);
             }
-            
         }
-        return -1;
+        return dp[amount] > amount ? -1 : dp[amount];
     }
 };
