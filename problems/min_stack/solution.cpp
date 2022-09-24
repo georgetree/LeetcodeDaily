31cd612f68ad023c2ae1c7313c48d663ae67fc53
georgetree <46710574+georgetree@users.noreply.github.com>
2022-09-24 17:10:51 +0000

    Sync LeetCode submission - Min Stack (cpp)

--- problems/min_stack/solution.cpp
+++ problems/min_stack/solution.cpp
@@ -1,25 +1,25 @@
 class MinStack {
-private:
-    vector<pair<int,int>> a;
 public:
+    vector<pair<int,int>> v;
     MinStack() {
+        
     }
     
     void push(int val) {
-        if(a.empty()) a.push_back({val,val});
-        else a.push_back({val,min(a.back().second,val)});
+        if(v.empty()) v.push_back({val,val});
+        else v.push_back({val,min(v.back().second,val)});
     }
     
     void pop() {
-        a.pop_back();
+        v.pop_back();
     }
     
     int top() {
-        return a.back().first;
+        return v.back().first;
     }
     
     int getMin() {
-        return a.back().second;
+        return v.back().second;
     }
 };
 
