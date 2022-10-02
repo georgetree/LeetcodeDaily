c103abf8b15fd603a0fe63eadc52ccbd79024268
georgetree <46710574+georgetree@users.noreply.github.com>
2022-10-02 08:46:19 +0000

    Sync LeetCode submission - Number of Dice Rolls With Target Sum (cpp)

--- problems/number_of_dice_rolls_with_target_sum/solution.cpp
+++ problems/number_of_dice_rolls_with_target_sum/solution.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     int numRollsToTarget(int n, int k, int target) {
-        int M = 1000000007;
-        vector<int> dp(target+1,0);
-        dp[0] = 1;
+        int M = 1e9 + 7;
+        vector<vector<int>> dp(n+1,vector<int>(target+1));
+        dp[0][0] = 1;
         for(int i=1; i<=n; i++){
-            vector<int> t(target + 1);
             for(int j=1; j<=k; j++){
                 for(int m=j; m<=target; m++){
-                    t[m] = (t[m] + dp[m-j]) % M;
+                    dp[i][m] = (dp[i][m] + dp[i-1][m-j]) % M;
                 }
             }
-            swap(dp,t);
         }
-        return dp[target];
+        return dp[n][target];
     }
 };
