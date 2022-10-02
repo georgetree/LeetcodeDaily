144068d93b1f835632c883fb08ca5c3bd72a4be9
georgetree <46710574+georgetree@users.noreply.github.com>
2022-10-02 18:33:34 +0000

    Sync LeetCode submission - Permutations (cpp)

--- problems/permutations/solution.cpp
+++ problems/permutations/solution.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<vector<int>> result;
-        vector<int> com;
-        vector<int> flag(nums.size(),0);
-        dfs(result,com,nums,flag);
-        return result;
+        vector<vector<int>> res;
+        vector<int> p;
+        helper(nums,res,p);
+        return res;
     }
-    void dfs(vector<vector<int>> &result, vector<int> &com, vector<int> &n, vector<int> &flag){
-        if(com.size()==n.size()){ 
-            result.push_back(com);
+    void helper(vector<int>& nums,vector<vector<int>> &res,vector<int>& p){
+        if(p.size()==nums.size()){
+            res.push_back(p);
             return;
         }
-        for(int i=0; i < n.size(); i++){
-            if(flag[i]) continue; 
-            flag[i] = 1;
-            com.push_back(n[i]);
-            dfs(result,com,n,flag);
-            com.pop_back();
-            flag[i] = 0;
+        for(int i=0; i<nums.size(); i++){
+            if(find(p.begin(),p.end(),nums[i])!=p.end()) continue;
+            p.push_back(nums[i]);
+            helper(nums,res,p);
+            p.pop_back();
         }
     }
 };
