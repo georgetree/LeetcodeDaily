ab5db48a5cc5a467fdcb1f151cf0f7882584416a
georgetree <46710574+georgetree@users.noreply.github.com>
2022-10-04 15:34:47 +0000

    Sync LeetCode submission - Subsets (cpp)

--- problems/subsets/solution.cpp
+++ problems/subsets/solution.cpp
@@ -2,17 +2,16 @@ class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> res;
-        vector<int> tmp;
-        dfs(res,tmp,nums,0);
+        vector<int> temp;
+        helper(nums,res,0,temp);
         return res;
     }
-    
-    void dfs(vector<vector<int>> &res, vector<int> &t, vector<int> &nums, int start){
-        res.push_back(t);
-        for(int i=start; i<nums.size(); i++){
-            t.push_back(nums[i]);
-            dfs(res,t,nums,i+1);
-            t.pop_back();
+    void helper(vector<int> &nums, vector<vector<int>> &res,int t, vector<int> temp){
+        res.push_back(temp);
+        for(int i=t; i<nums.size(); i++){
+            temp.push_back(nums[i]);
+            helper(nums,res,i+1,temp);
+            temp.pop_back();
         }
     }
 };
