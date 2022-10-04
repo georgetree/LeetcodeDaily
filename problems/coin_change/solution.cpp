724dada62345847e214e6454c6915b0ebdebf41c
georgetree <46710574+georgetree@users.noreply.github.com>
2022-10-04 20:30:12 +0000

    Sync LeetCode submission - Coin Change (cpp)

--- problems/coin_change/solution.cpp
+++ problems/coin_change/solution.cpp
@@ -3,12 +3,13 @@ public:
     int coinChange(vector<int>& coins, int amount) {
         vector<int> dp(amount+1,amount+1);
         dp[0] = 0;
-        for(int i=0; i<dp.size(); i++){
-            for(int coin :coins){
-                if(i - coin < 0) continue;
-                else dp[i] = min(dp[i],1+dp[i-coin]);
+        for(int i=1; i<=amount; i++){
+            for(auto coin:coins){
+                if(i-coin>=0){
+                    dp[i] = min(dp[i], dp[i-coin]+1);
+                }
             }
         }
-        return dp[amount] > amount ? -1 : dp[amount];
+        return dp[amount]>amount ? -1 : dp[amount];
     }
 };
