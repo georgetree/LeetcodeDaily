ce04fba60c415287cc17a99a4bb207d095d53300
georgetree <46710574+georgetree@users.noreply.github.com>
2022-10-06 18:45:46 +0000

    Sync LeetCode submission - Find K Closest Elements (cpp)

--- problems/find_k_closest_elements/solution.cpp
+++ problems/find_k_closest_elements/solution.cpp
@@ -1,17 +1,12 @@
 class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        priority_queue<pair<int,int>, vector<pair<int,int> >, greater<pair<int,int>>> q;
-        for(auto &a:arr){
-            q.push({abs(a-x),a});
+        int a = 0, b = arr.size()-1;
+        
+        while(arr.size()>k){
+            if(abs(x - arr.front()) > abs(arr.back()-x)) arr.erase(arr.begin());
+            else arr.pop_back();
         }
-        vector<int> res;
-        for(int i=0; i<k; i++){
-            res.push_back(q.top().second);
-            q.pop();
-        }
-        sort(res.begin(),res.end());
-        return res;
-            
+        return arr; 
     }
 };
