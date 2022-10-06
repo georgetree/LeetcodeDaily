7a6a25128e29953794e665af83c8b2600a22f743
georgetree <46710574+georgetree@users.noreply.github.com>
2022-10-06 20:11:27 +0000

    Sync LeetCode submission - Permutations (cpp)

--- problems/permutations/solution.cpp
+++ problems/permutations/solution.cpp
@@ -2,20 +2,21 @@ class Solution {
 public:
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> res;
-        vector<int> p;
-        helper(nums,res,p);
+        vector<int> temp;
+        helper(res,temp,nums);
         return res;
     }
-    void helper(vector<int>& nums,vector<vector<int>> &res,vector<int>& p){
-        if(p.size()==nums.size()){
-            res.push_back(p);
+    void helper(vector<vector<int>> &res, vector<int> temp, vector<int> &nums){
+        if(temp.size()==nums.size()){
+            res.push_back(temp);
             return;
         }
         for(int i=0; i<nums.size(); i++){
-            if(find(p.begin(),p.end(),nums[i])!=p.end()) continue;
-            p.push_back(nums[i]);
-            helper(nums,res,p);
-            p.pop_back();
+            if(find(temp.begin(),temp.end(),nums[i])!=temp.end()) continue;
+            temp.push_back(nums[i]);
+            helper(res,temp,nums);
+            temp.pop_back();
         }
+        
     }
 };
