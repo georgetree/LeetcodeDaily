1c38faa4ddabc91af0e6371d39b4362a0e90f59e
georgetree <46710574+georgetree@users.noreply.github.com>
2022-10-12 14:41:14 +0000

    Sync LeetCode submission - Largest Perimeter Triangle (cpp)

---
+++ problems/largest_perimeter_triangle/solution.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int largestPerimeter(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int res = 0;
+        for(int i=2; i<nums.size(); i++){
+            if(nums[i-1]+nums[i-2] > nums[i]) res = nums[i-1]+nums[i-2]+nums[i];
+        }
+        return res;
+    }
+};
