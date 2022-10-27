318b8be2e79fbd5015e4f266c2989aaaf49758a1
georgetree <46710574+georgetree@users.noreply.github.com>
2022-10-27 17:40:35 +0000

    Sync LeetCode submission - Find All Anagrams in a String (cpp)

--- problems/find_all_anagrams_in_a_string/solution.cpp
+++ problems/find_all_anagrams_in_a_string/solution.cpp
@@ -2,17 +2,16 @@ class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
         if(p.size()>s.size()) return {};
-        vector<int> m1(256,0),m2(256,0);
-        vector<int> res;
-        for (int i = 0; i < p.size(); ++i) {
-            ++m1[s[i]]; ++m2[p[i]];
-        }
 
-        if (m1 == m2) res.push_back(0);
-        for (int i = p.size(); i < s.size(); ++i) {
-            ++m1[s[i]]; 
-            --m1[s[i - p.size()]];
-            if (m1 == m2) res.push_back(i - p.size() + 1);
+        vector<int> m1(126,0),m2(126,0);
+        vector<int> res;
+        for(int i=0; i<p.size(); i++)
+            ++m1[s[i]], ++m2[p[i]];
+        if(m1==m2) res.push_back(0);
+        for(int i=p.size(); i<s.size(); i++){
+            ++m1[s[i]];
+            --m1[s[i-p.size()]];
+            if(m1==m2) res.push_back(i-p.size()+1);
         }
         return res;
     }
