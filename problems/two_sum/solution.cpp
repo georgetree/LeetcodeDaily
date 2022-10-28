57e8502d9ba2a79f972ef4e67fcb8fd44c005540
georgetree <46710574+georgetree@users.noreply.github.com>
2022-10-28 19:08:43 +0000

    Sync LeetCode submission - Two Sum (cpp)

--- problems/two_sum/solution.cpp
+++ problems/two_sum/solution.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int,int>m;
+        unordered_map<int,int> m;
         for(int i=0; i<nums.size(); i++){
-            if(m.find(target-nums[i])!=m.end())
-                return {i,m[target-nums[i]]};
+            if(m.count(target - nums[i])) return {i,m[target - nums[i]]};
             m[nums[i]] = i;
         }
         return {};
