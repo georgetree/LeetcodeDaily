607115b28521f57b76c70c62f9a8f37dbbea4dcc
georgetree <46710574+georgetree@users.noreply.github.com>
2022-10-29 09:13:53 +0000

    Sync LeetCode submission - Container With Most Water (cpp)

--- problems/container_with_most_water/solution.cpp
+++ problems/container_with_most_water/solution.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
+        int l = 0;
+        int r = height.size()-1;
         int res = 0;
-        int l = 0, r = height.size()-1;
-        while(l<r){
-            res = max(res,min(height[r],height[l])*(r-l));
-            height[l] < height[r] ? l++ : r--;
+        while (l<r) {
+            res = max(res, (r-l)* min(height[l],height[r]));
+            if(height[l]>=height[r]) r--;
+            else l++;
         }
         return res;
     }
