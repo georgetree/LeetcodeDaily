a93cb33ec09c048d0d444de73ad9c5b979cd17d1
georgetree <46710574+georgetree@users.noreply.github.com>
2022-11-03 19:26:23 +0000

    Sync LeetCode submission - Flood Fill (cpp)

--- problems/flood_fill/solution.cpp
+++ problems/flood_fill/solution.cpp
@@ -1,22 +1,24 @@
 class Solution {
+private:
+    vector<vector<int>> dir = {{0,1},{1,0},{0,-1},{-1,0}};
 public:
-    vector<vector<int>> dr{{0,1},{1,0},{0,-1},{-1,0}};
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
-        helper(image,sr,sc,newColor);
-        return image;
-    }
-    void helper(vector<vector<int>>& image, int sr, int sc, int newColor){
-        if(image[sr][sc]==newColor) return;
-        int t = image[sr][sc];
-        image[sr][sc] = newColor;
-        for(int i=0; i<4; i++){
-            int newx = sr + dr[i][0];
-            int newy = sc + dr[i][1];
-            if( newx>=0 && newx<image.size() && newy>=0 && newy<image[0].size()){
-                if( image[newx][newy] == t)
-                    helper(image,newx,newy,newColor);
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        if(image[sr][sc] == color) return image;
+        int m = image.size(), n = image[0].size();
+        queue<pair<int,int>> q;
+        q.push({sr,sc});
+        int c = image[sr][sc];
+        while(!q.empty()){
+            auto [x,y] = q.front();
+            q.pop();
+            image[x][y] = color;
+            for(int i=0; i<4; i++){
+                int nx = x + dir[i][0];
+                int ny = y + dir[i][1];
+                if(nx < 0 || nx >= m || ny < 0 || ny >= n || image[nx][ny]!=c) continue;
+                q.push({nx,ny});
             }
         }
-        
+        return image;
     }
 };
