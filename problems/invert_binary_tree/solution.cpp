bab28c844c26b2631d1ddd6973887d59777483a6
georgetree <46710574+georgetree@users.noreply.github.com>
2022-11-03 18:41:44 +0000

    Sync LeetCode submission - Invert Binary Tree (cpp)

--- problems/invert_binary_tree/solution.cpp
+++ problems/invert_binary_tree/solution.cpp
@@ -13,9 +13,9 @@ class Solution {
 public:
     TreeNode* invertTree(TreeNode* root) {
         if(root==nullptr) return nullptr;
-        TreeNode *t = root->right;
-        root->right = invertTree(root->left);
-        root->left = invertTree(t);
+        TreeNode* temp = root->left;
+        root->left = invertTree(root->right);
+        root->right = invertTree(temp);
         return root;
     }
 };
