b16bebe2b0b44d07286727662779b2cea166194e
georgetree <46710574+georgetree@users.noreply.github.com>
2022-11-04 19:49:43 +0000

    Sync LeetCode submission - Implement Queue using Stacks (cpp)

--- problems/implement_queue_using_stacks/solution.cpp
+++ problems/implement_queue_using_stacks/solution.cpp
@@ -1,40 +1,38 @@
 class MyQueue {
 public:
-    /** Initialize your data structure here. */
-    stack<int> st;
-    stack<int> st2;
+    stack<int> st1,st2;
     MyQueue() {
         
     }
     
-    /** Push element x to the back of queue. */
     void push(int x) {
-        while(!st.empty()){
-            st2.push(st.top());
-            st.pop();
-        }
-        st2.push(x);
-        while(!st2.empty()){
-            st.push(st2.top());
-            st2.pop();
-        }
+        st1.push(x);
     }
     
-    /** Removes the element from in front of queue and returns that element. */
     int pop() {
-        int t = st.top();
-        st.pop();
-        return t;
+        if(st2.empty()){
+            while(!st1.empty()){
+                st2.push(st1.top());
+                st1.pop();
+            }
+        }
+        int r = st2.top();
+        st2.pop();
+        return r;
     }
     
-    /** Get the front element. */
     int peek() {
-        return st.top();
+        if(st2.empty()){
+            while(!st1.empty()){
+                st2.push(st1.top());
+                st1.pop();
+            }
+        }
+        return st2.top();
     }
     
-    /** Returns whether the queue is empty. */
     bool empty() {
-        return st.empty();
+        return st1.empty() && st2.empty();
     }
 };
 
