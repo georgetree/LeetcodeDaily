f56cfe751f16493b668caadb7b7026cf53becc1d
georgetree <46710574+georgetree@users.noreply.github.com>
2022-11-06 08:38:01 +0000

    Sync LeetCode submission - Lowest Common Ancestor of a Binary Tree (cpp)

--- problems/lowest_common_ancestor_of_a_binary_tree/solution.cpp
+++ problems/lowest_common_ancestor_of_a_binary_tree/solution.cpp
@@ -10,12 +10,14 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(root==nullptr) return nullptr;
+        return helper(root,p,q);
+    }
+    TreeNode * helper(TreeNode *root, TreeNode *p, TreeNode *q){
+        if(root == nullptr) return nullptr;
         if(root->val == p->val || root->val == q->val) return root;
-        TreeNode *l = lowestCommonAncestor(root->left,p,q);
-        
-        TreeNode *r = lowestCommonAncestor(root->right,p,q);
+        TreeNode *l = helper(root->left,p,q);
+        TreeNode *r = helper(root->right,p,q);
         if(l&&r) return root;
-        return l!=nullptr ? l : r;
+        return l != nullptr ? l : r;
     }
 };
